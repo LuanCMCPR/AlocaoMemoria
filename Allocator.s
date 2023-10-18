98056d28bfba4e284cc1d2830f1e3a3bf4d0ccf1
LuanCMCPR <luancarlosmaia25@gmail.com>
2023-10-18 17:21:37 -0300

    Funcao setup_brk e dismiss_brk, criadas

--- Allocator.s
+++ Allocator.s
@@ -1,27 +1,44 @@
 .section .data
-    brkInicial: .quad 0
-    brkAtual: .quad 0
+    BRK_INICIAL: .quad 0
+    BRK_ATUAL: .quad 0
 
 .section .text
 .global _start
 
 setup_brk:
 # Obtém o endereço de brk
+    
+    pushq %rbp           
+    movq %rsp, %rbp     
+    
+    movq $12, %rax          # Syscall para o brk
+    movq $0 %rdi            # Retorna Endereço Atual  
+    syscall
 
-movq $12, %rax
-# rax 12 %rdi = 0 (retorna o endereco)
+    movq %rax, BRK_ATUAL
+    movq %rax, BRK_INICIAL
+
+    popq %rbp
+    ret
 
 dismiss_brk:
-
 # Restaura o endereço de brk
-# brkAtual = brkInicial
+    pushq %rbp
+    movq %rsp, %rbp
+
+    movq $12, %rax          # Syscall para o brk
+    movq BRK_INICIAL, %rdi  # Restaura o endereço de brk
+    syscall
+
+    popq %rbp
+    ret
 
 memory_alloc:
 
-# brkInicial = setup_brk();
+# BRK_INICIAL = setup_brk();
 
 # 1. Procura bloco livre com tamanho igual ou maior que a requisição
-    # a partir do brkInicial até o brkAtual, percorrer heap, checar tamanho e se esta livre
+    # a partir do BRK_INICIAL até o BRK_ATUAL, percorrer heap, checar tamanho e se esta livre
 
 # 2. Se encontrar, marca ocupação, utiliza os bytes necessários do bloco
     # se estiver livre e tiver tamanhor
@@ -34,7 +51,7 @@ memory_alloc:
     
 # 3. Se não encontrar, abre espaço para um novo bloco
     # se nao estiver nenhum livre
-        # brkAtual = brkAtual + bytes
+        # BRK_ATUAL = BRK_ATUAL + bytes
         # criar um novo bloco com tamanho bytes no final da heap
 # sempre retornar o endereco correspondente, do novo bloco
 
@@ -43,8 +60,17 @@ memory_free:
 # vai direto no ponteiro q passou, e muda USO para livre
 
 # conectar os blocos livres
-# segue do brkInicial ate o brkAtual, juntando o proximo livre, com o livre que estamos lendo
+# segue do BRK_INICIAL ate o BRK_ATUAL, juntando o proximo livre, com o livre que estamos lendo
 # tamanho temporario, conforme encontrar USO = 0 adicionar ao tamanho temporario, quando encontrar um USO = 1
     # seta o tamanho do bloco original
+    pushq %rbp
+    movq %rsp, %rbp
+
+    movq 16(%rbp),
+    movq $0, -16(%rbx) # Seta flag de uso do bloco como livre 
+    movq $0, %rax
+
+    popq %rbp
+    ret
 
 _start:
