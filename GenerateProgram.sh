014adecf7f477ea9ad6c2850bcde6d8c8fe0a5dc
LuanCMCPR <luancarlosmaia25@gmail.com>
2023-10-23 17:20:56 -0300

    Removendo Script Gerador

--- GenerateProgram.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-# Luan Carlos Maia Cruz - GRR 20203891
-# Felipe Augusto Dittert Noleto - GRR20205689
-
-# Script para gerar o executavel
-as Allocator.s -o Allocator.o -g
-gcc -g -Wall -c main.c -o main.o
-ld Allocator.o main.o -o Allocator  -dynamic-linker -lc
--- main.c
+++ main.c
@@ -10,33 +10,33 @@ int main()
     setup_brk();
 
     // a = memory_alloc(50);
-    // printHeap();
+    printHeap();
 
     // b = memory_alloc(100);
-    // printHeap();
+    printHeap();
 
     // c = memory_alloc(200);
-    // printHeap();
+    printHeap();
 
     // d = memory_alloc(250);
-    // printHeap();
+    printHeap();
 
     // e = memory_alloc(300);
-    // printHeap();
+    printHeap();
 
-    memory_free(b);
-    // printHeap();
+    // memory_free(b);
+    printHeap();
 
-    memory_free(d);
-    // printHeap();
+    // memory_free(d);
+    printHeap();
 
-    memory_free(a);
-    // printHeap();
+    // memory_free(a);
+    printHeap();
 
-    memory_free(c);
-    // printHeap();
+    // memory_free(c);
+    printHeap();
 
-    memory_free(e);
+    // memory_free(e);
     // printHeap();
 
     dismiss_brk();
